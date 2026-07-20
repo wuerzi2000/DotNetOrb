50b493d0b4676407d91879498694c995ccc9cfbf
Sascha Würzburg <29478730+wuerzi2000@users.noreply.github.com>
2026-07-20 09:50:15 +0200

    Fixes bidirectional GIOP callbacks failing without SSL due to an inheritance bug in IIOPProfile.

--- src/DotNetOrb.Core/IIOP/IIOPProfile.cs
+++ src/DotNetOrb.Core/IIOP/IIOPProfile.cs
@@ -668,7 +668,7 @@ namespace DotNetOrb.Core.IIOP
         }
 
 
-        public override List<ListenPoint> AsListenPoints()
+        public override IEnumerable<ListenPoint> AsListenPoints()
         {
             var result = new List<ListenPoint>();
 
